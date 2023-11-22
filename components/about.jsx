420e837f0cd1abdaec54b32099752a44b3dfb657
Jorge Matos <jisapjisap@gmail.com>
2023-11-22 15:58:37 +0100

    about

--- components/about.jsx
+++ components/about.jsx
@@ -239,7 +239,49 @@ const About = () => {
                     </div>
                   </div>
                 </TabsContent>
-                <TabsContent value='skills'>Skills info</TabsContent>
+                {/* skills */}
+                <TabsContent value='skills'>
+                  <div className="text-center xl:text-left">
+                    <h3 className="h3 mb-8">What I Use Everyday</h3>
+                    <div className="mb-16">
+                      <h4 className="text-xl font-semibold mb-2">Skills</h4>
+                      <div className="border-b border-border mb-4"></div>
+                      {/* skill list */}
+                      <div>
+                        {getData(skillData, 'skills').data.map((item, index) => {
+                          const { name } = item;
+                          return (
+                            <div className="w-/4 text-center xl:text-left mx-auto xl:mx-0" key={index}>
+                              <div className="font-medium">{name}</div>
+                            </div>
+                          )
+                        })}
+                      </div>
+                    </div>
+                    {/* tools */}
+                    <div>
+                      <h4 className="text-xl font-semibold mb-2 xl:text-left">Tools</h4>
+                      <div className="border-b border-border mb-4"></div>  
+                      {/* tool list */}
+                      <div className="flex gap-x-8 justify-center xl:justify-start">
+                        {getData(skillData, 'tools').data.map((item, index) => {
+                          const { imgPath } = item;
+                          return (
+                            <div key={index}>
+                              <Image
+                                src={imgPath}
+                                width={48}
+                                height={48}
+                                alt=""
+                                priority
+                              />
+                            </div>
+                          )
+                        })}
+                      </div>
+                    </div>
+                  </div>
+                </TabsContent>
               </div>
             </Tabs>
           </div>
