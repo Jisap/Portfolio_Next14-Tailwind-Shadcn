5a8df94b84ee13134c742519d4bcb89bb28d5135
Jorge Matos <jisapjisap@gmail.com>
2023-11-23 16:39:27 +0100

    services

--- app/page.jsx
+++ app/page.jsx
@@ -1,12 +1,14 @@
 import Hero from '@/components/Hero'
 import About from '@/components/about'
-import Image from 'next/image'
+import Services from '@/components/services'
+
 
 export default function Home() {
   return (
     <main className="">
       <Hero />
       <About />
+      <Services />
     </main>
   )
 }
