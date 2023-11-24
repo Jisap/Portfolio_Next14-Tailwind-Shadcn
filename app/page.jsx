773daac34f00b9ac69882f5dfd004212a27e8c9c
Jorge Matos <jisapjisap@gmail.com>
2023-11-24 17:01:42 +0100

    Work

--- app/page.jsx
+++ app/page.jsx
@@ -1,4 +1,5 @@
 import Hero from '@/components/Hero'
+import Work from '@/components/Work'
 import About from '@/components/about'
 import Services from '@/components/services'
 
@@ -9,6 +10,7 @@ export default function Home() {
       <Hero />
       <About />
       <Services />
+      <Work />
     </main>
   )
 }
---
+++ components/Work.jsx
@@ -0,0 +1,95 @@
+'use client'
+
+import Link from "next/link"
+import { Button } from "./ui/button"
+
+import 'swiper/css'
+import 'wiper/css/pagination'
+
+import { Pagination } from 'swiper/modules'
+import ProjectCard from "./ProjectCard"
+
+const projectDAta = [
+  {
+    image: '/work/3.png',
+    category: 'react js',
+    name: 'Nexa Website',
+    description: 'Lorem impsum dolor sit amet consectetur adipisicing elit. Cunque, quis.',
+    link: '/',
+    github: '/',
+  },
+  {
+    image: '/work/4.png',
+    category: 'react js',
+    name: 'Solstice Website',
+    description: 'Lorem impsum dolor sit amet consectetur adipisicing elit. Cunque, quis.',
+    link: '/',
+    github: '/',
+  },
+  {
+    image: '/work/2.png',
+    category: 'Next js',
+    name: 'Lumina Website',
+    description: 'Lorem impsum dolor sit amet consectetur adipisicing elit. Cunque, quis.',
+    link: '/',
+    github: '/',
+  },
+  {
+    image: '/work/1.png',
+    category: 'Next js',
+    name: 'Evolve Website',
+    description: 'Lorem impsum dolor sit amet consectetur adipisicing elit. Cunque, quis.',
+    link: '/',
+    github: '/',
+  },
+  {
+    image: '/work/3.png',
+    category: 'next js',
+    name: 'Ignite Website',
+    description: 'Lorem impsum dolor sit amet consectetur adipisicing elit. Cunque, quis.',
+    link: '/',
+    github: '/',
+  },
+  {
+    image: '/work/4.png',
+    category: 'next js',
+    name: 'Envision Website',
+    description: 'Lorem impsum dolor sit amet consectetur adipisicing elit. Cunque, quis.',
+    link: '/',
+    github: '/',
+  },
+  {
+    image: '/work/1.png',
+    category: 'fullstack',
+    name: 'Serenity Website',
+    description: 'Lorem impsum dolor sit amet consectetur adipisicing elit. Cunque, quis.',
+    link: '/',
+    github: '/',
+  },
+  {
+    image: '/work/3.png',
+    category: 'fullstack',
+    name: 'Nova Website',
+    description: 'Lorem impsum dolor sit amet consectetur adipisicing elit. Cunque, quis.',
+    link: '/',
+    github: '/',
+  },
+  {
+    image: '/work/2.png',
+    category: 'fullstack',
+    name: 'Zenith Website',
+    description: 'Lorem impsum dolor sit amet consectetur adipisicing elit. Cunque, quis.',
+    link: '/',
+    github: '/',
+  },
+]
+
+const Work = () => {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default Work
