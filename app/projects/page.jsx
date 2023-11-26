0a852b77e9667b87d54977ce79aad0e364497d69
Jorge Matos <jisapjisap@gmail.com>
2023-11-26 19:01:48 +0100

    projects

--- app/projects/page.jsx
+++ app/projects/page.jsx
@@ -2,7 +2,7 @@
 
 
 import ProjectCard from "@/components/ProjectCard"
-import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs"
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 import { useState } from "react"
 
 const projectData = [
@@ -88,6 +88,11 @@ const Projects = () => {
   const [categories, setCategories] = useState(uniqueCategories);
   const [category, setCategory] = useState('all projects');
 
+  const filteredProjects = projectData.filter( project => {
+    // Si category === 'all projects' devolvemos todos los projects, sino filtramos por categoría
+    return category === 'all projects' ? project : project.category === category 
+  });
+
   return (
     <section className="min-h-screen pt-12">
       <div className="container mx-auto">
@@ -95,7 +100,7 @@ const Projects = () => {
           My projects
         </h2>
         {/* tabs */}
-        <Tabs defaultValue={category}>
+        <Tabs defaultValue={category} className="mb-24 xl:mb-48">
           <TabsList 
             className="w-full grid h-full md:grid-cols-4 lg:max-w-[640px] mb-12 mx-auto
             md:border dark:border-none"
@@ -113,6 +118,16 @@ const Projects = () => {
               )
             })}
           </TabsList>
+          {/* tabs content */}
+          <div className="text-lg xl:mt-8 grid grid-cols-1 lg:grid-cols-3 gap-4">
+            {filteredProjects.map((project, index) => {
+              return (
+                <TabsContent value={category} key={index}>
+                  <ProjectCard project={project} />
+                </TabsContent>
+              )
+            })}
+          </div>
         </Tabs>
       </div>
     </section>
