feb0194b9b0c68989c397a4965275983b34f979f
Jorge Matos <jisapjisap@gmail.com>
2023-11-27 11:21:18 +0100

    projects

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -8,7 +8,7 @@ const Contact = () => {
     <section>
       <div className="container mx-auto">
         {/* text & illustration */}
-        <div>
+        <div className="grid xl:grid-cols-2 pt-12 xl:h-[480px] mb-6 xl:mb-24">
           {/* text */}
           <div className="flex flex-col justify-center">
             <div className="flex items-center gap-x-4 text-primary text-lg mb-4">
@@ -16,10 +16,40 @@ const Contact = () => {
               Say Hello 👏
             </div>
             <h1 className="h1 max-w-md mb-8">let's Work Together.</h1>
+            <p className="subtitle max-w-[400px]">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.
+            </p>
           </div>
+          {/* illustration */}
+          <div className="hidden xl:flex w-full bg-contact_illustration_light dark:bg-contact_illustration_dark
+          bg-contain bg-top bg-no-repeat"
+          ></div>
         </div>
         {/* info text & form */}
-        <div>info text & form</div>
+        <div className="grid xl:grid-cols-2 mb-24 xl:mb-32">
+          {/* info text */}
+          <div className="flex flex-col gap-y-4 xl:gap-y-14 mb-12 xl:mb-24 text-base xl:text-lg">
+            {/* mail */}
+            <div className="flex items-center gap-x-8">
+              <MailIcon size={18} className="text-primary"/>
+              <div>youremail@gamil.com</div>
+            </div>
+            {/* address */}
+            <div className="flex items-center gap-x-8">
+              <HomeIcon size={18} className="text-primary" />
+              <div>8420 Pine Avenue, Meadowville MC 842</div>
+            </div>
+            {/* phone */}
+            <div className="flex items-center gap-x-8">
+              <PhoneCall size={18} className="text-primary" />
+              <div>+44 123 546 7890</div>
+            </div>
+          </div>
+          {/* form */}
+          <form>
+            form
+          </form>
+        </div>
       </div>
     </section>
   )
